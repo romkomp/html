fd445c45ab9d71b33f192bef5015f0eb0a934556
Roman Sitko <53574854+romkomp@users.noreply.github.com>
2023-03-03 20:59:48 +0000

    carousel

--- js/clients-carousel.js
+++ js/clients-carousel.js
@@ -6,14 +6,40 @@
         '<div><img src="img/client_LisaWilson.jpg" alt="Lisa Wilson"></div>'
     ];
 
+    let currentSlide = 0;
+    
     function renderSlides(slides) {
         const slidesContainer = document.querySelector('.clients-carousel__slides');
-        slidesContainer.innerHTML = '';
-
-        for(let slide of slides) {
-            slidesContainer.innerHTML += slide;
-        }
-
+        slidesContainer.innerHTML = slides[currentSlide];
+        if (window.innerWidth > 768) {
+            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+            slidesContainer.innerHTML += slides[secondSlide];
+            if (window.innerHTML > 900) {
+                const thirdSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+                slidesContainer.innerHTML += slides[thirdSlide];
+            }
+        } 
     }
 
-}) ();
+    function nextSlide() {
+        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+        renderSlides(slides);
+    }
+
+    function prevSlide() {
+        currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1;
+        renderSlides(slides);
+    }
+
+    // setInterval(nextSlide, 3000);
+    renderSlides(slides);
+
+    //buttons
+
+    const nextButton = document.querySelector('.clients-carousel__next');
+    nextButton.addEventListener('click', nextSlide);
+
+    const prevButton = document.querySelector('.clients-carousel__prev');
+    nextButton.addEventListener('click', prevSlide);
+
+})();
